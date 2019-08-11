bd368d85c5daac26b42ab1c6017e46854743c8cb
kronos <roobin.bhandari@gmail.com>
2019-08-11 21:18:19 +0545

    init

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private taskService: TasksService) { }
+
+    @Get()
+    getTasks(): Task[] {
+        return this.taskService.getAllTasks();
+    }
+    @Post()
+    createTask(@Body("title") title: string, @Body("description") description: string, ): Task {
+
+        return this.taskService.createTask(title, description);
+
+
+
+    }
+}
