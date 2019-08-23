e20a6083f777b7794ac6809a7400d6f920e95efd
kronos <roobin.bhandari@gmail.com>
2019-08-23 15:47:36 +0545

    added helmet and compression

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(compression());
+  app.use(helmet());
   await app.listen(8000);
 }
 bootstrap();
