17fac21e095db591b490b28e2254199eea2211ca
kronos <roobin.bhandari@gmail.com>
2019-08-29 16:03:06 +0545

    updated dependencies

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { classToPlain , Transform, plainToClass } from "class-transformer";
-class User {
-    @Transform(value => value.toUpperCase(), { toClassOnly: true })
-    id: string;
-}
-
-const test= {id: 'skhdsj'};
-
-console.log(plainToClass(User, test));
